4b116330f8406a1ee13cd1005f44fccb98af633f
Jaewon Lee <jwlee010222@gmail.com>
2023-03-28 13:48:43 +0900

    first commit

---
+++ 나의 앱.swiftpm/MyApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FloatingWordsView()
+        }
+    }
+//    func PlaygroundPage.current.setLiveView(FrustrationGameView())
+}
