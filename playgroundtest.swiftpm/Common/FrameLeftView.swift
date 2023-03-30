f0be1e7399610d62daa6fe271d682221ae93088a
sei <vanism7557@gmail.com>
2023-03-31 01:45:53 +0900

    fix: frame left

--- playgroundtest.swiftpm/Common/FrameLeftView.swift
+++ playgroundtest.swiftpm/Common/FrameLeftView.swift
@@ -17,54 +17,45 @@ struct FrameLeftView: View {
                 .onEnded({ value in
                     soundManager.play(sound: .button, volume: 1.0)
                     
-                    let x = value.location.x
-                    let y = value.location.y
+                    let x = value.location.x, y = value.location.y
                     
-                    if (x > 46) && (x < 124) && (y > 650) && (y < 730) {
+                    if (46...125).contains(x),
+                       (650...730).contains(y) {
                         image = Frame.ButtonClicked.none
-                    } else if (x > 46) && (x < 124) && (y > 550) && (y < 635) {
-                        image = Frame.ButtonClicked.one
-                        sceneNum = 1
-                    } else if (x > 46) && (x < 124) && (y > 460) && (y < 550) {
-                        image = Frame.ButtonClicked.two
-                        sceneNum = 2
-                    } else if (x > 46) && (x < 124) && (y > 360) && (y < 450) {
-                        image = Frame.ButtonClicked.three
-                        sceneNum = 3
-                    } else if (x > 46) && (x < 124) && (y > 270) && (y < 350) {
-                        image = Frame.ButtonClicked.four
-                        sceneNum = 4
-                    } else if (x > 46) && (x < 124) && (y > 180) && (y < 260) {
-                        image = Frame.ButtonClicked.five
-                        sceneNum = 5
-                    } else if (x > 46) && (x < 124) && (y > 85) && (y < 170) {
-                        image = Frame.ButtonClicked.six
-                        sceneNum = 6
                     }
                     
                 })
-//                    .onChanged({ value in
-//                        print(value.location)
-//                        let x = value.location.x
-//                        let y = value.location.y
-//
-//                        if value.location.x > 46 && value.location.x < 124
-//                            && value.location.y > 650 && value.location.y < 730 {
-//                            image = Frame.ButtonClicked.rew
-//                        } else if (x > 46) && (x < 124) && (y > 550) && (y < 635) {
-//                            image = Frame.ButtonClicked.one
-//                        } else if (x > 46) && (x < 124) && (y > 460) && (y < 550) {
-//                            image = Frame.ButtonClicked.two
-//                        } else if (x > 46) && (x < 124) && (y > 360) && (y < 450) {
-//                            image = Frame.ButtonClicked.three
-//                        } else if (x > 46) && (x < 124) && (y > 270) && (y < 350) {
-//                            image = Frame.ButtonClicked.four
-//                        } else if (x > 46) && (x < 124) && (y > 180) && (y < 260) {
-//                            image = Frame.ButtonClicked.five
-//                        } else if (x > 46) && (x < 124) && (y > 85) && (y < 170) {
-//                            image = Frame.ButtonClicked.six
-//                        }
-//                    })
+                    .onChanged({ value in
+                        print(value.location)
+                        let x = value.location.x, y = value.location.y
+                        
+                        soundManager.play(sound: .button, volume: 1.0)
+
+                        switch (x, y) {
+                        case (46..<124, 650..<730):
+                            image = Frame.ButtonClicked.rew
+                        case (46..<124, 550..<635):
+                            image = Frame.ButtonClicked.one
+                        default:
+                            return
+                        }
+                        if value.location.x > 46 && value.location.x < 124
+                            && value.location.y > 650 && value.location.y < 730 {
+                            image = Frame.ButtonClicked.rew
+                        } else if (x > 46) && (x < 124) && (y > 550) && (y < 635) {
+                            image = Frame.ButtonClicked.one
+                        } else if (x > 46) && (x < 124) && (y > 460) && (y < 550) {
+                            image = Frame.ButtonClicked.two
+                        } else if (x > 46) && (x < 124) && (y > 360) && (y < 450) {
+                            image = Frame.ButtonClicked.three
+                        } else if (x > 46) && (x < 124) && (y > 270) && (y < 350) {
+                            image = Frame.ButtonClicked.four
+                        } else if (x > 46) && (x < 124) && (y > 180) && (y < 260) {
+                            image = Frame.ButtonClicked.five
+                        } else if (x > 46) && (x < 124) && (y > 85) && (y < 170) {
+                            image = Frame.ButtonClicked.six
+                        }
+                    })
             )
     }
 }
